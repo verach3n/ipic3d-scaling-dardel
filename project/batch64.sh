5bf25fd1f3995153ffce6bd4425f2769f111b309
perttu <perttu_1995@hotmail.com>
2023-05-28 03:08:16 +0200

    n=64,n=512 batch output/input files

---
+++ project/batch64.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -l
+#SBATCH -J 64_DD2356_proj
+#SBATCH -t 00:10:00
+#SBATCH -A edu23.DD2356
+# Number of nodes
+#SBATCH -p main
+#SBATCH --ntasks-per-node=64
+#SBATCH --nodes=1
+#SBATCH -e error_file_64.e
+srun -n 64 ./iPIC3D ../inputfiles/testGEM3Dsmall_64.inp > 64.out
