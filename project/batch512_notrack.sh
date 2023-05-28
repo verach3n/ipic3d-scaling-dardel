031a0087b98802a0aeec30a23c80fa050e46d35f
perttu <perttu_1995@hotmail.com>
2023-05-28 06:39:30 +0200

    n=512,1024 input/output files

---
+++ project/batch512_notrack.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -l
+#SBATCH -J 512_DD2356_proj
+#SBATCH -t 00:10:00
+#SBATCH -A edu23.DD2356
+# Number of nodes
+#SBATCH -p main
+#SBATCH --ntasks-per-node=128
+#SBATCH --nodes=4
+#SBATCH -e error_file_512_notrack.e
+srun ./iPIC3D ../inputfiles/testGEM3Dsmall_512_notrack.inp > 512_notrack.out
