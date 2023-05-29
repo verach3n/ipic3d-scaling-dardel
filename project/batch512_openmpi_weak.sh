1d4e92b59df3746cdac04a7f50a03478f8e3d885
Perttu Jääseläinen <perttu_1995@hotmail.com>
2023-05-29 15:51:43 +0200

    assignment3 missing entries, init openmpi batch runs

---
+++ project/batch512_openmpi_weak.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J OpenMPI
+#SBATCH -p main
+#SBATCH -A edu23.DD2356
+#SBATCH --time=00:10:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=128
+#SBATCH --output=slurm-run-%j.out
+#SBATCH --error=slurm-run-%j.err
+
+module unload cray-mpich
+# module load openmpi/4.1.3-cpeGNU-22.06 
+
+export PATH=/pdc/software/22.06/eb/software/openmpi/4.1.3-cpeGNU-22.06/bin/:$PATH
+export LD_LIBRARY_PATH=/pdc/software/22.06/eb/software/openmpi/4.1.3-cpeGNU-22.06/lib/:$LD_LIBRARY_PATH
+
+CC=mpicc CXX=mpicxx cmake ..
+
+make
+
+mpirun -n 512 ./iPIC3D ../inputfiles/512weak.inp > 512_openmpi_weak.out
+
